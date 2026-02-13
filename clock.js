d4b00bd39e174d2544ebc2ef74265ca95a119dfe
larleevee <lanavalentine1889@gmail.com>
2026-02-13 19:15:44 +0000

    new homepage

---
+++ clock.js
@@ -0,0 +1,10 @@
+function updateClock() {
+  const clock = document.getElementById('bar-clock');
+  const now = new Date();
+  const h = String(now.getHours()).padStart(2, '0');
+  const m = String(now.getMinutes()).padStart(2, '0');
+  const s = String(now.getSeconds()).padStart(2, '0');
+  clock.textContent = `${h}:${m}:${s}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
