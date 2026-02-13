d7fa7999c6415d4928d5514ddbbe7f59a06bae0e
larleevee <lanavalentine1889@gmail.com>
2026-02-13 23:39:14 +0000

    video popup

--- menu.js
+++ menu.js
@@ -1,24 +1,56 @@
+function makeDraggable(windowEl, titlebarEl) {
+  let isDragging = false, offsetX = 0, offsetY = 0;
+  titlebarEl.addEventListener('mousedown', function(e) {
+    isDragging = true;
+    offsetX = e.clientX - windowEl.offsetLeft;
+    offsetY = e.clientY - windowEl.offsetTop;
+    document.body.style.userSelect = 'none';
+  });
+  document.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+      windowEl.style.left = (e.clientX - offsetX) + 'px';
+      windowEl.style.top = (e.clientY - offsetY) + 'px';
+      windowEl.style.right = 'auto';
+      windowEl.style.bottom = 'auto';
+    }
+  });
+  document.addEventListener('mouseup', function() {
+    isDragging = false;
+    document.body.style.userSelect = '';
+  });
+}
+
 const menuClose = document.getElementById('menuClose');
 const menuWindow = document.getElementById('menuWindow');
-menuClose.addEventListener('click', () => {
-  menuWindow.style.display = 'none';
-});
-
 const menuTitlebar = document.getElementById('menuTitlebar');
-let isDragging = false, offsetX = 0, offsetY = 0;
-menuTitlebar.addEventListener('mousedown', function(e) {
-  isDragging = true;
-  offsetX = e.clientX - menuWindow.offsetLeft;
-  offsetY = e.clientY - menuWindow.offsetTop;
-  document.body.style.userSelect = 'none';
-});
-document.addEventListener('mousemove', function(e) {
-  if (isDragging) {
-    menuWindow.style.left = (e.clientX - offsetX) + 'px';
-    menuWindow.style.top = (e.clientY - offsetY) + 'px';
+if (menuWindow && menuTitlebar && menuClose) {
+  makeDraggable(menuWindow, menuTitlebar);
+  menuClose.addEventListener('click', () => {
+    menuWindow.style.display = 'none';
+  });
+}
+
+const bobGif = document.querySelector('img[src="assets/buttons/bob.gif"]');
+const videoMenu = document.getElementById('videoMenu');
+const videoMenuClose = document.getElementById('videoMenuClose');
+const videoMenuTitlebar = document.getElementById('videoMenuTitlebar');
+const announcementVideo = document.getElementById('announcementVideo');
+
+if (videoMenu && videoMenuTitlebar && videoMenuClose) {
+  makeDraggable(videoMenu, videoMenuTitlebar);
+  videoMenuClose.addEventListener('click', () => {
+    videoMenu.style.display = 'none';
+    if (announcementVideo) {
+      announcementVideo.pause();
+    }
+  });
+  if (bobGif) {
+    bobGif.addEventListener('click', () => {
+      videoMenu.style.display = '';
+      if (announcementVideo) {
+        announcementVideo.currentTime = 0;
+        announcementVideo.play();
+      }
+    });
   }
-});
-document.addEventListener('mouseup', function() {
-  isDragging = false;
-  document.body.style.userSelect = '';
-});
+}
